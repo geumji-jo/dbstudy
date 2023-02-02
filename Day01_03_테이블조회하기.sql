07c594629ec960cdc951dcefa883d493540ad6a8
tikitaca <sweet3761@naver.com>
2023-02-02 15:47:06 +0900

    2023-02-02

--- Day01_03_테이블조회하기.sql
+++ Day01_03_테이블조회하기.sql
@@ -11,6 +11,12 @@
         [WHERE 조건식]
         [GROUP BY 그룹화할칼럼 [HAVING 그룹조건식]]
         [ORDER BY 정렬할칼럼 정렬방식]
+    6. 순서
+        ④ SELECT 조회할칼럼, 조회할칼럼, 조회할칼럼, ... 
+        ① FROM 테이블이름
+        ② [WHERE 조건식]
+        ③ [GROUP BY 그룹화할칼럼 [HAVING 그룹조건식]]
+        ⑤ [ORDER BY 정렬할칼럼 정렬방식]
 */
 
 /*
@@ -20,3 +26,47 @@
     3. 모든 세부 작업이 성공하면 COMMIT하고, 하나라도 실패하면 모든 세부 작업의 취소를 진행한다.
         (All or Nothing)
 */
+
+-- 조회 실습.
+
+-- 1. 사원 테이블에서 사원명 조회하기
+-- 1) 기본 방식
+SELECT ENAME
+FROM EMP;
+
+-- 2) 오너명시하기(테이블을 가지고 있는 계정)
+SELECT ENAME
+  FROM SCOTT.EMP;
+
+-- 3) 테이블 명시하기(칼럼을 가지고 있는 테이블)
+SELECT EMP.ENAME
+  FROM EMP;
+
+-- 4) 테이블 별명 지정하기
+SELECT E.ENAME
+  FROM EMP E;     -- EMP 테이블의 별명을 E로 부여한다. AS(ALIAS)를 사용할 수 없다.
+
+-- 5) 칼럼 별명 지정하기
+SELECT E.ENAME AS 사원명     --E.ENAME 칼럼의 별명을 '사원명'으로 부여한다. AS(ALIAS)를 사용할 수 있다.
+  FROM EMP E;
+
+  
+  
+-- 2. 사원테이블의 모든 칼럼 조회하기  
+-- 1) * 활용하기(SELECT*(에스터리스크)는 모든 칼럼을 의미한다.)
+SELECT *         -- 불려 가기 싫으면 사용 금지!
+FROM EMP;
+
+-- 2) 모든 칼럼 직접 작성하기
+SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
+FROM EMP;
+
+
+-- 3. 동일한 데이터는 한 번만 조회하기
+--    DISTINCT
+SELECT DISTINCT JOB
+    FROM EMP;
+
+
+
+
